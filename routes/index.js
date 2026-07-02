ae2255fb91bc5442a068ce2837bdcb8bff56ef25
Toru Kurahashi <140052461+toru-kurahashi@users.noreply.github.com>
2026-07-02 07:00:22 +0000

    サインイン機能を作成しよう

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  console.log(`isAuth: ${isAuth}`);
   knex("tasks")
     .select("*")
     .then(function (results) {
@@ -36,5 +39,6 @@ router.post('/', function (req, res, next) {
 });
 
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
 
 module.exports = router;
