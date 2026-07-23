65d332c6df38a317a779d1c9f51bcf62473a08f4
Toru Kurahashi <140052461+toru-kurahashi@users.noreply.github.com>
2026-07-23 06:26:01 +0000

    passport認証の動作確認をしよう

--- config/passport.js
+++ config/passport.js
@@ -7,18 +7,20 @@ const cookieSession = require("cookie-session");
 const secret = "secretCuisine123";
 
 module.exports = function (app) {
-  passport.serializeUser(function (user, done) {
-    done(null, user.id);
-  });
+passport.serializeUser(function(user, done) {
+  console.log("serializeUser");
+  done(null, user.id);
+});
 
-  passport.deserializeUser(async function (id, done) {
-    try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch (error) {
-      done(error, null);
-    }
-  });
+passport.deserializeUser(async function (id, done) {
+  console.log("deserializeUser");
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
 
   passport.use(new LocalStrategy({
       usernameField: "username",
